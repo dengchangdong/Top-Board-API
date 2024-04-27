097b94cebfa8e2e94656e4ac90be3e2b790d64d6
Deng Changdong <dengchangdong@gmail.com>
2024-04-27 11:59:26 +0800

    Update Home.tsx

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -7,34 +7,9 @@ const Home: FC = () => {
     <Layout title="Trands API">
       <main className="home">
         <div className="title">
-          <h1 className="title-text">DailyHot API</h1>
-          <span className="title-tip">服务已正常运行</span>
-        </div>
-        <div class="control">
-          <button id="test-button">
-            <svg
-              className="btn-icon"
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              viewBox="0 0 24 24"
-            >
-              <path
-                fill="currentColor"
-                d="M7.71 6.71a.996.996 0 0 0-1.41 0L1.71 11.3a.996.996 0 0 0 0 1.41L6.3 17.3a.996.996 0 1 0 1.41-1.41L3.83 12l3.88-3.88c.38-.39.38-1.03 0-1.41m8.58 0a.996.996 0 0 0 0 1.41L20.17 12l-3.88 3.88a.996.996 0 1 0 1.41 1.41l4.59-4.59a.996.996 0 0 0 0-1.41L17.7 6.7c-.38-.38-1.02-.38-1.41.01M8 13c.55 0 1-.45 1-1s-.45-1-1-1s-1 .45-1 1s.45 1 1 1m4 0c.55 0 1-.45 1-1s-.45-1-1-1s-1 .45-1 1s.45 1 1 1m4-2c-.55 0-1 .45-1 1s.45 1 1 1s1-.45 1-1s-.45-1-1-1"
-              />
-            </svg>
-            <span className="btn-text">测试接口</span>
-          </button>
+          <h1 className="title-text">服务已正常运行</h1>
         </div>
       </main>
-      {html`
-        <script>
-          document.getElementById("test-button").addEventListener("click", () => {
-            window.location.href = "/trands/all";
-          });
-        </script>
-      `}
     </Layout>
   );
 };
